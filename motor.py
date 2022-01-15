ddb5c59b05d6b03f7b51cd82d0fd662e83fde8c3
Volodymyr Fursenko <v.fursenko@gmail.com>
2022-01-15 23:36:41 +0200

    -fix

--- motor.py
+++ motor.py
@@ -5,22 +5,22 @@ left = Motor(17, 18)
 right  = Motor(4, 14)
 
 while True:
-    for i in range(0, 10):
+    for i in range(0, 6):
         left.forward(i*0.1)
         right.forward(i*0.1)
         sleep(0.4)
         
-    for i in range(0, 10)
+    for i in range(0, 6)
         left.forward((6 - i) * 0.1)
         right.forward((6 - i) * 0.1)
         sleep(0.4)
         
-    for i in range(0, 10):
+    for i in range(0, 6):
         left.backward(i*0.1)
         right.backward(i*0.1)
         sleep(0.4)
         
-    for i in range(0, 10)
+    for i in range(0, 6)
         left.backward((6 - i) * 0.1)
         right.backward((6 - i) * 0.1)
         sleep(0.4)
