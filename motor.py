a8d4560b3565f1aaa54a500ecd8861e41ae8e773
Volodymyr Fursenko <v.fursenko@gmail.com>
2022-01-15 23:35:04 +0200

    -init file

---
+++ motor.py
@@ -0,0 +1,26 @@
+from gpiozero import Motor
+from time import sleep
+
+left = Motor(17, 18)
+right  = Motor(4, 14)
+
+while True:
+    for i in range(0, 10):
+        left.forward(i*0.1)
+        right.forward(i*0.1)
+        sleep(0.4)
+        
+    for i in range(0, 10)
+        left.forward((6 - i) * 0.1)
+        right.forward((6 - i) * 0.1)
+        sleep(0.4)
+        
+    for i in range(0, 10):
+        left.backward(i*0.1)
+        right.backward(i*0.1)
+        sleep(0.4)
+        
+    for i in range(0, 10)
+        left.backward((6 - i) * 0.1)
+        right.backward((6 - i) * 0.1)
+        sleep(0.4)
